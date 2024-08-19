75f60143b3dd333f85998d6a4c204e817136bc28
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-08-19 13:30:17 +0100

    support older IOS versions on our test suite

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // npm install --save-dev @types/node required to find this module
 import { fileURLToPath, URL } from 'url'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
@@ -14,5 +15,13 @@ export default defineConfig({
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
+  },
+  // to do : document/capture this : we configure build to meet older IOS version 12.
+  //         IOS v12 was simply displaying a blank screen.
+  //         by default, vite builds for ES Modules (so es2022)
+  //         https://vitejs.dev/guide/build.html#browser-compatibility
+  //         https://vitejs.dev/config/build-options.html#build-target
+  build: {
+    target: "es2015"
   },
 })
