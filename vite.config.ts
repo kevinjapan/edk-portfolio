e95e31efa48173d420566eff0f053fae26fe453f
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-08-20 13:30:38 +0100

    update comments

--- vite.config.ts
+++ vite.config.ts
@@ -16,11 +16,9 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-  // to do : document/capture this : we configure build to meet older IOS version 12.
-  //         IOS v12 was simply displaying a blank screen.
-  //         by default, vite builds for ES Modules (so es2022)
-  //         https://vitejs.dev/guide/build.html#browser-compatibility
-  //         https://vitejs.dev/config/build-options.html#build-target
+  // we configure build to meet older IOS version 12.
+  // IOS v12 was simply displaying a blank screen, by default, vite builds for ES Modules (so es2022)
+  // https://vitejs.dev/guide/build.html#browser-compatibility https://vitejs.dev/config/build-options.html#build-target
   build: {
     target: "es2015"
   },
