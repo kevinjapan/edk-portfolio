640ffb972f6b37fb05a4b1eddb2c7118fa5163b1
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-11-07 12:51:11 +0000

    Create Paragraph.vue

---
+++ src/components/ProjectSection/Paragraph/Paragraph.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref, watchEffect } from 'vue'
+import markdownit from 'markdown-it'
+
+const props = defineProps<{
+    text: string
+}>()
+
+// we handle basic markdown in the text contents of our json data files
+const md = markdownit()
+const rawHtml = ref('')
+
+
+watchEffect(() => {    
+    rawHtml.value = md.render(props.text)
+})
+
+
+</script>
+
+<template>
+    <p v-html="rawHtml"></p>
+</template>
