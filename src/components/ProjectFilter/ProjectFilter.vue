1a61d362577036e497a761c6000e7dd12887a560
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-11-15 12:53:25 +0000

    fix spelling

--- src/components/ProjectFilter/ProjectFilter.vue
+++ src/components/ProjectFilter/ProjectFilter.vue
@@ -38,7 +38,7 @@ const num_with_filter = (filter: string) : number => {
       </li>
       <li class="skill_tag">
          <a class="tech_tag" @click="set_filter('latest')" :class="{selected_filter: filter === 'latest'}">
-            Lastest ({{ num_with_filter('latest') }})</a>
+            Latest ({{ num_with_filter('latest') }})</a>
       </li>
       <li class="skill_tag">
          <a class="tech_tag" @click="set_filter('javascript')" :class="{selected_filter: filter === 'javascript'}">
