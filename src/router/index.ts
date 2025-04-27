a608e8eacf961b1d1bf8ae46927448378855a6a1
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2025-04-27 11:38:49 +0100

    tidy code

--- src/router/index.ts
+++ src/router/index.ts
@@ -19,13 +19,13 @@ const router = createRouter({
          path: '/projects/:project_slug',
          component: () => import('../views/ProjectView.vue') 
       },
-      {
-         name: 'about',
-         path: '/about',
-         // route level code-splitting - lazy-loaded when the route is visited
-         // generates a separate chunk (About.[hash].js) for this route
-         component: () => import('../views/AboutView.vue')
-      },
+      // {
+      //    name: 'about',
+      //    path: '/about',
+      //    // route level code-splitting - lazy-loaded when the route is visited
+      //    // generates a separate chunk (About.[hash].js) for this route
+      //    component: () => import('../views/AboutView.vue')
+      // },
       { 
          name: 'notfound',
          path: '/:pathMatch(.*)*',
